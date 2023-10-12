2d94d2c13c73ad937391b4eae548ca73d082614f
Michael Cook <mcook4728@gmail.com>
2023-10-11 23:57:08 -0700

    implemented ddns container

--- pve1/docker/ddns/ddns.sh
+++ pve1/docker/ddns/ddns.sh
@@ -1,52 +1,64 @@
-#!/bin/bash
+#!/bin/ash
 
 # Get IP addr
-ip4=$(curl -s -4 icanhazip.com)
+echo "Beginning Cloudflare DDNS Job"
 
-echo "$ip4"
+ip4=$(curl -s -4 icanhazip.com)
 
 # Regular expression for IPv4
 regex="^(?:[0-9]{1,3}\.){3}[0-9]{1,3}$"
 
 if [[ "$ip4" =~ "$regex" ]]
 then
-    echo "No valid IP address found"
+    printf "A valid IP address for this device could not be resolved.\n\n"
     exit 1
 fi
 
 baseUrl="https://api.cloudflare.com"
 contentType="Content-Type: application/json"
 
-$API_KEY=$(cat $API_KEY_FILE)
+API_KEY=$(cat $API_KEY_FILE)
 bearerAuthHeader="Authorization: Bearer $API_KEY"
 
-url="$HOSTNAME.$SUBZONE"
-
-echo "$url"
+echo "Fetching DNS Zone information for $ZONE"
 
 # Get DNS Zones
-zoneId=$(curl -s --request GET --url "$baseUrl/client/v4/zones?name=$SUBZONE" -H "$contentType" -H "$bearerAuthHeader" | jq -r --arg SUBZONE "$SUBZONE" '.result[] | select(.name==$SUBZONE) | .id' | cat)
+zoneId=$(curl -s --request GET --url "$baseUrl/client/v4/zones?name=$ZONE" -H "$contentType" -H "$bearerAuthHeader" | jq -r --arg ZONE "$ZONE" '.result[] | select(.name==$ZONE) | .id' | cat)
+echo "Zone Id: $zoneId"
 
-echo "$SUBZONEId"
-
-if [[ -z $SUBZONEId ]]
+if [[ -z $zoneId ]]
 then
-    echo "Could not resolve a zone id"
+    printf "Could not resolve a zone id. This script does not support initializing zones.\n\n"
     exit 1
 fi
 
-# Get DNS Records
-recordId=$(curl -s --request GET --url "$baseUrl/client/v4/zones/$SUBZONEId/dns_records?type=A&name=$url" -H "$contentType" -H "$bearerAuthHeader" | jq -r --arg url "$url" '.result[] | select(.name=$url) | .id' | cat)
+url="$SUBZONE.$ZONE"
+echo "Fetching DNS Record information for $url"
 
-echo "$recordId"
+# Get DNS Records
+dnsRecordResponse=$(curl -s --request GET --url "$baseUrl/client/v4/zones/$zoneId/dns_records?type=A&name=$url" -H "$contentType" -H "$bearerAuthHeader")
+
+recordId=$(echo "$dnsRecordResponse"| jq -r --arg url "$url" '.result[] | select(.name=$url) | .id' | cat)
+echo "DNS Record Id: $recordId"
 
 if [[ -z $recordId ]]
 then
-    echo "Could not resolve a record id. A new record will be created"
+    echo "Could not resolve a DNS record. A new record will be created."
+else
+    echo "Found an existing DNS record. Determining if the record needs to be updated."
+    currentIp=$(echo "$dnsRecordResponse"| jq -r --arg url "$url" '.result[] | select(.name=$url) | .content' | cat)
+
+    if [[ $currentIp == $ip4 ]]
+    then 
+        printf "DNS record content matches current record. No need to Update.\n\n"
+        exit 0
+    else
+        echo "DNS record content is oudated. The record will be updated."
+    fi
 fi
 
 requestMethod=$([ -z "$recordId" ] && echo "POST" || echo "PUT")
-requestUrl="$baseUrl/client/v4/zones/$SUBZONEId/dns_records$([ -z "$recordId" ] && echo "" || echo "/$recordId")"
+requestUrl="$baseUrl/client/v4/zones/$zoneId/dns_records$([ -z "$recordId" ] && echo "" || echo "/$recordId")"
 requestBody='{
     "content": "'"$ip4"'",
     "name": "'"$url"'",
@@ -58,5 +70,11 @@ requestBody='{
 # Create or Update DNS Record
 result=$(curl -s --request "$requestMethod" --url "$requestUrl" -H "$contentType" -H "$bearerAuthHeader" -d "$requestBody" | jq -r '.success')
 
-echo "$result"
-exit $([ "$result" == "true" ] && echo 0 || echo 1)
+if [[ $result == "true" ]]
+then
+    printf "Successfully created or updated the DNS record for $url\n\n"
+    exit 0
+else
+    printf "Failed to create or update the DNS Record for $url\n\n"
+    exit 1
+fi
